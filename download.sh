f84e7fd986ac8e1e6b2aa25d9494d15d3013ed96
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-02-11 09:55:46 +0100

    download script

---
+++ download.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+FILENAME=scala-library-$1.jar
+
+rm class -rf
+rm bcode -rf
+wget http://scala-tools.org/repo-snapshots/org/scala-lang/scala-library/2.8.0-SNAPSHOT/$FILENAME && \
+unzip $FILENAME -d class && \
+java -cp out/production/scala-disassembled:../treegraph/project/boot/scala-2.8.0.Beta1/lib/scala-library.jar:/usr/lib/jvm/java-6-openjdk/lib/tools.jar RecursiveJavaP && \
+git add bcode/* && \
+git ci -m "$FILENAME"
+
+rm $FILENAME
