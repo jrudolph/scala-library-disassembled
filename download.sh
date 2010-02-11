29e65a14d4bbd6ae6d35aaafa5f1626c113a81dd
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-02-11 10:21:35 +0100

    track library.properties as well

--- download.sh
+++ download.sh
@@ -7,6 +7,7 @@ git rm bcode -rf
 echo $FILENAME > bcode/version &&\
 wget http://scala-tools.org/repo-snapshots/org/scala-lang/scala-library/2.8.0-SNAPSHOT/$FILENAME && \
 unzip $FILENAME -d class && \
+cp class/library.properties bcode && \
 java -cp out/production/scala-disassembled:../treegraph/project/boot/scala-2.8.0.Beta1/lib/scala-library.jar:/usr/lib/jvm/java-6-openjdk/lib/tools.jar RecursiveJavaP && \
 git add bcode/* && \
 git ci -m "$FILENAME"
