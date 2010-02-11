9a9137c2e7d7e1bf86dc035888aa5eed1d4a9ac7
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-02-11 09:57:05 +0100

    put version file into bcode

--- download.sh
+++ download.sh
@@ -4,6 +4,8 @@ FILENAME=scala-library-$1.jar
 
 rm class -rf
 rm bcode -rf
+mkdir bcode && \
+echo $FILENAME > bcode/version &&\
 wget http://scala-tools.org/repo-snapshots/org/scala-lang/scala-library/2.8.0-SNAPSHOT/$FILENAME && \
 unzip $FILENAME -d class && \
 java -cp out/production/scala-disassembled:../treegraph/project/boot/scala-2.8.0.Beta1/lib/scala-library.jar:/usr/lib/jvm/java-6-openjdk/lib/tools.jar RecursiveJavaP && \
